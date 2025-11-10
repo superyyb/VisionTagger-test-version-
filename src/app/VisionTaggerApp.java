50abec2270de9d1b5e374323db8e15c141d5c142
annanil <li.n1@northeastern.edu>
2025-11-10 11:22:51 -0800

    add entry point for the project

---
+++ src/app/VisionTaggerApp.java
@@ -0,0 +1,32 @@
+package app;
+
+import model.DetectionResult;
+import model.Image;
+import service.ImageAnalyzerService;
+import service.MockRekognitionService;
+
+public class VisionTaggerApp {
+  public static void main(String[] args) {
+    System.out.println("Starting VisionTagger Demo...\n");
+    
+    try {
+      // Create image analyzer service
+      ImageAnalyzerService analyzer = new MockRekognitionService();
+      
+      // Create an image to analyze
+      Image image = new Image("testuser", "/path/to/image.jpg", "Test description");
+      
+      // Analyze the image
+      DetectionResult result = analyzer.detect(image);
+      
+      // Display results
+      System.out.println(result);
+      
+    } catch (Exception e) {
+      System.err.println("Demo failed");
+      e.printStackTrace();
+    }
+  }
+
+  // TODO: Abstract input and output streams to allow for different input sources and output sinks.
+}
